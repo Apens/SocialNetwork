093f95b41f7c9d3d488d3595680f7b80dd541f83
Apens <Apenscorp@gmail.com>
2020-06-05 18:55:13 +0200

    fonctinalitée "add friend" créée

--- profile.php
+++ profile.php
@@ -9,7 +9,20 @@ if (isset($_GET['profile_username'])){
     $user_array = mysqli_fetch_array($user_details_query);
 
     $num_friends = substr_count($user_array['friend_array'], ",")-1;
-} else
+}
+if (isset($_POST['remove_friend'])){
+    $user = new User($con, $userLoggedIn);
+    $user->removeFriend($username);
+}
+if (isset($_POST['add_friend'])){
+    $user = new User($con, $userLoggedIn);
+    $user->sendRequest($username);
+}
+if (isset($_POST['respond_request'])){
+    header("Location: request.php");
+}
+
+
 ?>
 
         <style>
@@ -33,7 +46,7 @@ if (isset($_GET['profile_username'])){
                     <?= "Friends: ".$num_friends; ?>
                 </p>
             </div>
-            <form action="<?= $username;?>">
+            <form action="<?= $username;?>" method="POST">
                 <?php
                     $profile_user_obj = new User($con, $username);
 
@@ -45,7 +58,16 @@ if (isset($_GET['profile_username'])){
                     if($userLoggedIn != $username){
 
                         if($logged_in_user_obj->isFriend($username)){
-                            echo '<input type="submit" name="remove_friend" class="text-danger" value="Remove Friend"><br>';
+                            echo '<input type="submit" name="remove_friend" class="danger" value="Remove Friend"><br>';
+                        }
+                        elseif ($logged_in_user_obj->didReceiveRequest($username)) {
+                            echo '<input type="submit" name="respond_request" class="warning" value="Respond to Request"><br>';
+                        }
+                        elseif ($logged_in_user_obj->didSendRequest($username)) {
+                            echo '<input type="submit" name="" class="default" value="Request Sent"><br>';
+                        }
+                        else {
+                            echo '<input type="submit" name="add_friend" class="success" value="Add Friend"><br>';
                         }
 
                     }
